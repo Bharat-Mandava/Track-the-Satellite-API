d7cf4ce97e1dd5871a65c1641492e8289468af86
Bharat-Mandava <43178562+Bharat-Mandava@users.noreply.github.com>
2019-07-11 22:32:07 -0400

    Adding more comments

--- app.js
+++ app.js
@@ -62,13 +62,12 @@ let allowZoom = true;
         
         // const coordinateUrl = `https://api.wheretheiss.at/v1/coordinates/${latitude},${longitude}`;
         const coordinateUrl = `https://api.wheretheiss.at/v1/coordinates/37.795517,-122.393693`;
-        
+       // const coordinateUrl = `https://api.wheretheiss.at/v1/coordinates/${latitude},${longitude}` 
 
-
+            
         async function satellitecountry () {
             const satelliteresponse = await fetch (coordinateUrl);
             const satellitedata = await satelliteresponse.json();
-             console.log(satellitedata.country_code)
             const countryCode = satellitedata.country_code;
             document.getElementById('countrycode').textContent = countryCode;
         }
@@ -85,4 +84,4 @@ let allowZoom = true;
      mySatellite()
    
     // To track the position of Satellite from time to time or stop the app from tracking the satellite location, adjust the time(in milliseconds) and enable/disable the setInterval method below
-    // setInterval(mySatellite, 1000);
+     setInterval(mySatellite, 1000);
